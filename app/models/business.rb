ce27ca5586b9e01f3e732273e385e5a4cff7d39f
Gaurav <gaurav@vinsol.com>
2015-03-16 12:36:15 +0530

    Naming issue fix

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,7 +1,7 @@
 class Business < ActiveRecord::Base
   include Elasticsearch::Model
 
-  Vinsol_Location = { lat: 28.644894, lon: 77.170619 }
+  VinsolLocation = { lat: 28.644894, lon: 77.170619 }
 
   settings index: { number_of_shards: 1, number_of_replicas: 0 } do
     mapping do 
@@ -21,7 +21,7 @@ class Business < ActiveRecord::Base
         filter: {
           geo_distance: {
             distance: distance + 'km',
-            location: Vinsol_Location
+            location: VinsolLocation
           }
         }
       }
